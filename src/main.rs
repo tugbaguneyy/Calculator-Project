6e8688f523cc671c7419c3cbc6908b35aea256e3
tugbaguneyy <200202089@kocaeli.edu.tr>
2023-07-27 16:58:50 +0300

    Added input control

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
 use std::io::{self, Write};
+
 fn main() {
-    println!("Hello! Please enter a operation number:(1.Add 2.Divide 3.Multiply 4.Substact)");
-    let mut op_input = String::new(); //kullanıcıdan işlemi almak için
-    io::stdin().read_line(&mut op_input);
-    std::io::stdout().flush();//ekranı güncellemek için
-    
-    println!("Enter the first number");//kullanıcıdan birinci sayıyı almak için
-    let mut num1_input = String::new(); 
-    io::stdin().read_line(&mut num1_input);
-    std::io::stdout().flush();//ekranı güncellemek için
-    
-    println!("Enter the second number");//kullanıcıdan ikinci sayıyı almak için
-    let mut num2_input = String::new(); 
-    io::stdin().read_line(&mut num2_input);
-    std::io::stdout().flush();//ekranı güncellemek için
+    println!("Hello! Please enter an operation number: (1.Add 2.Divide 3.Multiply 4.Substract)");
 
-    //convert işlem
-    let op: i16 = op_input.trim().parse().expect("Invalid number");
-    let num1: f64 = num1_input.trim().parse().expect("Invalid number");
-    let num2: f64 = num2_input.trim().parse().expect("Invalid number");
-    println!("{} {}",&num1,&num2);
+    let op: i16 = loop {
+        let mut op_input = String::new();
+        io::stdin().read_line(&mut op_input).expect("Failed to read input");
 
-    //girilen işleme göre operation oluşturma
+        // parse() ile dönüşüm denemek
+        match op_input.trim().parse::<i16>() {
+            Ok(num) => break num, // Geçerli bir sayı dönüştüyse döngüyü sonlandır
+            Err(_) => println!("Invalid operation number. Please try again."),
+        };
+    };
+
+    println!("Enter the first number:");
+    let num1: f64 = loop {
+        let mut num1_input = String::new();
+        io::stdin().read_line(&mut num1_input).expect("Failed to read input");
+
+        // parse() ile dönüşüm denemek
+        match num1_input.trim().parse::<f64>() {
+            Ok(num) => break num, // Geçerli bir sayı dönüştüyse döngüyü sonlandır
+            Err(_) => println!("Invalid number. Please try again."),
+        };
+    };
+
+    println!("Enter the second number:");
+    let num2: f64 = loop {
+        let mut num2_input = String::new();
+        io::stdin().read_line(&mut num2_input).expect("Failed to read input");
+
+        // parse() ile dönüşüm denemek
+        match num2_input.trim().parse::<f64>() {
+            Ok(num) => break num, // Geçerli bir sayı dönüştüyse döngüyü sonlandır
+            Err(_) => println!("Invalid number. Please try again."),
+        };
+    };
+
+    // Girilen işleme göre operation oluşturma
     let result = match op {
         1 => Operation::Add(num1, num2),
         2 => Operation::Divide(num1, num2),
@@ -30,7 +47,6 @@ fn main() {
         _ => panic!("Invalid operation selection."),
     };
     println!("Sonuç: {}", result.calculate());
-
 }
 
 enum Operation {
@@ -39,6 +55,7 @@ enum Operation {
     Multiply(f64, f64),
     Divide(f64, f64),
 }
+
 impl Operation {
     fn calculate(&self) -> f64 {
         match self {
